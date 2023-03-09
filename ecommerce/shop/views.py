e086fefdc327c5231a66f74536b5cf2f3f85dcfe
Goudjou Borel <goudjouborel@gmail.com>
2023-03-09 13:09:41 +0100

    designer le site avec bootstrap et afficher les produits

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Product 
 # Create your views here.
+
+def index (request):
+    product_object = Product.objects.all()
+    return render(request, 'shop/index.html', {'product_object' : product_object})
